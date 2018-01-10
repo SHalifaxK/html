9885e8e7d1b834080517d1c6c7f7d08ef0779297
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-01-10 10:49:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+function inf() {
+    var name = document.getElementById("F_L_Name").value;
+    var email = document.getElementById("email").value;
+    var passwd = document.getElementById("pass_1").value;
+    
+    alert("Your info"+"\n"+name+"\n"+email+"\n"+passwd);
+}
